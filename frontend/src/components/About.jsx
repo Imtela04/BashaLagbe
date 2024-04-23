e6c0ce0f14d16d42f9c7131bf1cbfde760e2a40d
Imtela04 <85889248+Imtela04@users.noreply.github.com>
2024-04-23 22:06:21 +0600

    created an About page

---
+++ frontend/src/components/About.jsx
@@ -0,0 +1,47 @@
+import React from 'react'
+
+export default function About(){
+    return (
+        <div className='relative h-screen bg-black'>
+            <img
+                className="w-full h-full object-cover"
+                src="https://images.unsplash.com/photo-1515542706656-8e6ef17a1521?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                alt="imagename"
+                
+            />
+            <div className='relative h-screen bg-black'>
+                <div className='p-20'>
+                <h1  className='text-3xl font-bold mb-4 text-slate-800'>About BashaLagbe!</h1>
+                <p className='mb-4 text-white'> BashaLagbe! Your One-Stop Solution for Student Accommodation</p>
+                <p className='mb-4 text-white'>Welcome to BashaLagbe! We understand the struggles students face in finding the perfect place to live. That's why we created a platform specifically designed to meet the needs of students seeking accommodation.  Here at BashaLagbe, you can:</p>
+                
+                <p className='mb-4 text-white'>Find a variety of student-friendly accommodations: Browse through listings for apartments, shared rooms, dorms, and more, all conveniently located near universities and colleges.</p>
+                <p className='mb-4 text-white'>Unique Bidding System: Don't just settle for the listed price! Our innovative bidding system allows you to place bids on available rooms, potentially securing your ideal place at a price that fits your budget.</p>
+                <p className='mb-4 text-white'>Connect directly with landlords and tenants: Communicate directly with landlords or potential roommates through our secure messaging system, eliminating the need for intermediaries.</p>
+                <p className='mb-4 text-white'>Safe and Secure Platform: We prioritize safety and security. All listings are reviewed before being posted, and our messaging system ensures clear communication.</p>
+                
+                <p className='mb-4 text-white'>Benefits for Students:</p>
+                
+
+                    <p className='mb-4 text-white'>-Find the perfect place: With a wide range of listings, you're sure to find an accommodation that suits your needs and budget.</p>
+                    <p className='mb-4 text-white'>-Competitive Renting: The bidding system empowers you to potentially secure a place at a lower price.</p>
+                    <p className='mb-4 text-white'>-Convenience and Efficiency: Quickly search for listings, connect with landlords, and manage your search all in one place.</p>
+                    <p className='mb-4 text-white'>-Peace of Mind: Our secure platform ensures a safe and transparent experience.</p>
+                    <p className='mb-4 text-white'>Benefits for Landlords:</p>
+                
+                    <p className='mb-4 text-white'>-Reach a wider audience: Advertise your student accommodation listings directly to potential tenants who are actively seeking housing.</p>
+                    <p className='mb-4 text-white'>-Efficient Communication: Easily connect and communicate with interested students through our messaging system.</p>
+                    <p className='mb-4 text-white'>-Flexible Pricing: The bidding system allows you to potentially receive higher offers for your property.</p>
+                
+                    
+                    <p className='mb-4 text-white'>Join the BashaLagbe Community Today!</p>
+                    
+                    <p className='mb-4 text-white'>Whether you're a student searching for the perfect place to live or a landlord with a student-friendly property to rent out, BashaLagbe is the ideal platform for you. Sign up today and experience the convenience and flexibility of our unique student accommodation system!</p>
+
+                </div>
+                
+            </div>
+            
+        </div>
+    )
+}
