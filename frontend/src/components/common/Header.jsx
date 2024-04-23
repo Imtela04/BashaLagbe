fb5ca38da82ed658042e5609a4e73ad99a4d81aa
Imtela04 <85889248+Imtela04@users.noreply.github.com>
2024-04-23 22:20:31 +0600

    turned the logo into a home button

--- frontend/src/components/common/Header.jsx
+++ frontend/src/components/common/Header.jsx
@@ -35,6 +35,14 @@ const Header = () => {
                 <img className="h-24 w-auto" src={Img} alt="Your Company" />
               </Link>
             </div>
+            <div>
+              <Link to='/about'>
+                <li className='text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium'>
+                  About
+                </li>
+            </Link>
+            </div>
+            
             
             {/* Links */}
             
